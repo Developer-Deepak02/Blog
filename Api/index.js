17d36b955a7075923770deb6d6c90c2062ed5862
Developer-Deepak02 <db23806@gmail.com>
2025-08-04 21:37:16 +0530

    feat: implement post creation functionality with Mongoose model and route

--- Api/index.js
+++ Api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./Routes/user.route.js";
 import authRoutes from "./Routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import postRoutes from "./Routes/post.route.js";
 
 // database connection and server setup
 dotenv.config();
@@ -26,9 +27,9 @@ app.listen(PORT, () => {
 app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
 // middleware for handling errors
-
 app.use((err, req, res, next) => {
 	const statusCode = err.statusCode || 500;
 	const message = err.message || "Internal Server Error";
